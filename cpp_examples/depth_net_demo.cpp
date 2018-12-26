a22926fc4b080b063163323c6effd3f33e2bad3e
ryo <ryo.cv.0311@gmail.com>
2018-12-27 02:06:58 +0900

    implemented cpp interface

---
+++ cpp_examples/depth_net_demo.cpp
@@ -0,0 +1,119 @@
+#include <torch/torch.h>
+#include <torch/script.h> // One-stop header.
+
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <cmath>
+
+#include <opencv2/opencv.hpp>
+
+cv::Mat forwardProcess(const std::shared_ptr<torch::jit::script::Module>& module,
+                       const std::pair<cv::Mat,cv::Mat>& pm_images);
+
+int main(int argc, char* argv[]) {
+
+  if (argc != 4){
+    std::cout << "usage : this.out [/path/to/model.pt] [/path/to/image0] [/path/to/image1]";
+    std::cout << std::endl;
+    return -1;
+  }
+
+
+  // Loading your model
+  const std::string s_model_name = argv[1];
+  std::cout << " >>> Loading " << s_model_name;
+  std::shared_ptr<torch::jit::script::Module> module = torch::jit::load(s_model_name);
+
+  assert(module != nullptr);
+  std::cout << " ... Done! " << std::endl;;
+
+  // Loading your images
+  const std::string s_image_name0 = argv[2];
+  const std::string s_image_name1 = argv[3];
+
+  std::cout << " >>> Loading " << s_image_name0 << " and " << s_image_name1;
+  auto pm_images = std::make_pair(cv::imread(s_image_name0,1),cv::imread(s_image_name1, 1));
+  std::cout << " ... Done! " << std::endl;
+
+  cv::Mat m_depth = forwardProcess(module, pm_images);
+
+  std::cout << " >>> Demo has finised !" << std::endl;
+
+  return 0;
+}
+
+
+cv::Mat forwardProcess(const std::shared_ptr<torch::jit::script::Module>& module,
+                       const std::pair<cv::Mat,cv::Mat>& pm_images)
+{
+  // define tensor dimention you will generate from cv::Mat
+  const int channel = pm_images.first.channels();
+  const int height = pm_images.first.rows;
+  const int width = pm_images.first.cols;
+  std::vector<int64_t> dims{static_cast<int64_t>(1), // 1
+                            static_cast<int64_t>(channel * 2), // 6
+                            static_cast<int64_t>(height), // h
+                            static_cast<int64_t>(width)}; // w
+
+  std::cout << " >>> [" << channel 
+            << " x " << height 
+            << " x " << width 
+            << "]" << std::endl;
+
+  cv::Mat mf_input = cv::Mat::zeros(height*channel*2, width, CV_32FC1);
+
+  cv::Mat m_bgr0[channel],m_bgr1[channel], mf_rgb_rgb[channel*2];
+  cv::split(pm_images.first, m_bgr0);
+  for(int i = 0; i < 3; i++) {
+    m_bgr0[i].convertTo(mf_rgb_rgb[2-i], CV_32FC1, 1.0/255.0, -0.5); 
+  }
+
+  cv::split(pm_images.second, m_bgr1);
+  for(int i = 0; i < 3; i++) {
+    m_bgr1[i].convertTo(mf_rgb_rgb[5-i], CV_32FC1, 1.0/255.0, -0.5); 
+  }
+
+  for (int i = 0; i < 6; i++) {
+    mf_rgb_rgb[i].copyTo(mf_input.rowRange(i*height, (i+1)*height));
+  }
+
+  mf_input /= 0.2;
+
+  at::TensorOptions options(at::kFloat);
+  at::Tensor input_tensor = torch::from_blob(mf_input.data, at::IntList(dims), options);
+
+  at::Tensor output_tensor = module->forward({input_tensor}).toTensor();
+  std::cout << " >>> [" << output_tensor.size(0)
+            << " x " << output_tensor.size(1)
+            << " x " << output_tensor.size(2)
+            << "]" << std::endl;
+  cv::Mat m_output(cv::Size(output_tensor.size(1), output_tensor.size(2)), CV_32FC1, output_tensor.data<float>());
+  cv::Mat m_depth = m_output.clone();
+  cv::Mat m_upscaled_depth;
+  cv::resize(m_depth, m_upscaled_depth, cv::Size(width, height), 0, 0);
+  const float max_value = 100.0; // this is determined by the original python
+  cv::Mat m_arranged_depth = 255.0*m_upscaled_depth/max_value; 
+  for (int i = 0; i < m_arranged_depth.rows; i++) {
+    for (int j = 0; j < m_arranged_depth.cols; j++) {
+      if(m_arranged_depth.at<float>(i,j) > 255.0) {
+        m_arranged_depth.at<float>(i,j) = 255.0;
+      } 
+      else if (m_arranged_depth.at<float>(i,j) < 0.0) {
+        m_arranged_depth.at<float>(i,j) = 0.0;
+      }
+    }
+  }
+  m_arranged_depth.convertTo(m_arranged_depth, CV_8U); 
+  // array = (255*tensor.squeeze().numpy()/max_value).clip(0, 255).astype(np.uint8)
+  cv::Mat m_color_map;
+  cv::applyColorMap(m_arranged_depth, m_color_map, cv::COLORMAP_RAINBOW);
+  
+  cv::imshow("depth-color-map(press q to quit)", m_color_map);
+  for(;;) {
+    if(cv::waitKey(50)=='q')
+      break;
+  }
+  return m_arranged_depth;
+}
+
---
+++ export_model.py
@@ -0,0 +1,47 @@
+import torch
+
+from scipy.misc import imread, imsave, imresize
+import numpy as np
+from path import Path
+import argparse
+from tqdm import tqdm
+
+import torch.nn.functional as F
+from models import DepthNet
+from util import tensor2array
+
+parser = argparse.ArgumentParser(description='Export model using torch.jit.trace.',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("--img-height", default=512, type=int, help="Image height")
+parser.add_argument("--img-width", default=512, type=int, help="Image width")
+parser.add_argument("--cuda", default=False, type=bool, help="use GPU")
+
+parser.add_argument("--pretrained", required=True, type=str, help="pretrained DepthNet path")
+
+@torch.no_grad()
+def main():
+    args = parser.parse_args()
+    if args.cuda:
+        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        mode = "gpu" if torch.cuda.is_available() else "cpu"
+    else:
+        device = torch.device("cpu")
+        mode = "cpu"
+
+    weights = torch.load(args.pretrained)
+    depth_net = DepthNet(batch_norm=weights['bn'],
+                         depth_activation=weights['activation_function'],
+                         clamp=weights['clamp']).to(device)
+    depth_net.load_state_dict(weights['state_dict'])
+    depth_net.eval()
+
+    h = args.img_height 
+    w = args.img_width
+    # input-tensor-size = 1 x 6 x h x w
+    traced_net = torch.jit.trace(depth_net, torch.rand(1, 6, h, w).to(device))
+    traced_net.save("DepthNet_h{}_w{}_{}.pt".format(h,w,mode))
+    print("DepthNet_h{}_w{}_{}.pt is exported".format(h,w,mode))
+
+if __name__ == '__main__':
+    main()
